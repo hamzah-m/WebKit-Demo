279eb3af70d6580a2de1cc5d94ea0ca41cc31ac4
Hamzah Mugharbil <Hamzah@hamzahs-mbp.lan>
2017-08-11 00:35:42 +0300

    Initial Commit

---
+++ WebKit Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebKit Demo
+//
+//  Created by Hamzah Mugharbil on 8/11/17.
+//  Copyright © 2017 Hamzah Mugharbil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebKit Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebKit Demo
+//
+//  Created by Hamzah Mugharbil on 8/11/17.
+//  Copyright © 2017 Hamzah Mugharbil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
